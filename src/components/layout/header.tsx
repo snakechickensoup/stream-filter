69afb9a99f589ca0c0d5932f001767972975e8c1
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-06-14 18:19:20 +0900

    chore: 아이콘 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,11 @@
+import { X } from 'lucide-react';
+
 const Header = () => {
   return (
     <header className='window-header'>
       <h2 className='window-title'>stream filter</h2>
       <button type='button' aria-label='close' className='button close-button'>
-        <span className='icon icon-close'></span>
+        <X size={20} />
       </button>
     </header>
   );
--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,9 +1,11 @@
+import { Camera } from 'lucide-react';
+
 const Nav = () => {
   return (
     <div className='camera'>
       <span>... </span>
       <button className='button camera-button' type='button' aria-label='camera'>
-        <span className='icon icon-camera'></span>
+        <Camera size={16} />
         <span className='text'>촬영</span>
       </button>
     </div>
